950f407f391c88697ee61d35209c70fa7beedd9d
Stabien <bastien.piedallu@gmail.com>
2020-09-25 12:56:18 +0200

    component DisplayImages working with categorie filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
-import DisplayImages from './DisplayImages.js'
+import DisplayImages from './DisplayImages.js';
 import '../css/App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'jquery/dist/jquery.js'
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categorie: ""
+    }
+  }
+
+  componentDidMount() {
+    $('li').on('click', (e) => {
+      this.setState({
+        categorie: e.target.id
+      });
+    });
+  }
+
   render() {
     return (
       <main className="page bg-white">
         <h1>GALERIE</h1>
-        <div className="navbar navbar-expand-lg container">
-            <nav className="collapse navbar-collapse justify-content-end">
-              <ul className="navbar-nav">
-                <li className="nav-item">TOUS</li>
-                <li className="nav-item">MODERNES</li>
-                <li className="nav-item">CONTEMPORAINS</li>
-                <li className="nav-item">PORTRAITS</li>
-                <li className="nav-item"i>PAYSAGES</li>
-              </ul>
-            </nav>
+        <div className="navbar navbar-expand-lg container" id="nav_container">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <nav className="collapse navbar-collapse justify-content-end" id="navbar-content">
+            <ul className="navbar-nav">
+              <li className="nav-item nav-link" id="tous">TOUS</li>
+              <li className="nav-item nav-link" id="modernes">MODERNES</li>
+              <li className="nav-item nav-link" id="contemporains">CONTEMPORAINS</li>
+              <li className="nav-item nav-link" id="portraits">PORTRAITS</li>
+              <li className="nav-item nav-link" id="paysages">PAYSAGES</li>
+            </ul>
+          </nav>
         </div>
         <div className="container">
-          <DisplayImages />
+          <DisplayImages categorie={this.state.categorie}/>
         </div>
       </main>
     );
--- src/components/DisplayImages.js
+++ src/components/DisplayImages.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 class DisplayImages extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       myImages: []
     }
@@ -14,16 +14,19 @@ class DisplayImages extends Component {
       .then(result => {
         this.setState({
           myImages: result
-        })
+        });
       });
   }
 
   render() {
-    const listItems = this.state.myImages.map(item => (
-      <div className="col-lg-4 col-md-6">
-        <img src={item.path} alt="images"/>
-      </div>
-    ));
+    const listItems = this.state.myImages.map(item => {
+      if (this.props.categorie == "tous" || item.categorie == this.props.categorie) 
+        return(
+          <div key={item.id} className="col-lg-4 col-md-6">
+            <img src={item.path} alt="images"/>
+          </div>
+        );
+    });
     return <div className="row">{listItems}</div>;
   }
 }
