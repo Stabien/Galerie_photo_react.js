575139283892062f70a6018afcfbf06977dc25fb
Stabien <bastien.piedallu@gmail.com>
2020-10-02 22:13:19 +0200

    string refs replaced using createRef

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -6,26 +6,31 @@ class NavItem extends Component {
     this.state = {
       activeNavItem: ""
     }
+    this.navItemRef = [React.createRef()];
   }
 
   changeStyleNavItem(e) {
-    for (var ref in this.refs) {
-      this.refs[ref].style.background = "white";
-      this.refs[ref].style.color = "black";
+    for (var ref in this.navItemRef) {
+      this.navItemRef[ref].style.background = "white";
+      this.navItemRef[ref].style.color = "black";
     }
     e.target.style.background = "#0EC5DC";
     e.target.style.color = "white";
+    this.setState({
+      activeNavItem: e.target.id
+    });
   }
 
   render() {
     const categories = ["tous", "modernes", "contemporains", "portraits", "paysages"];
-    const listItems = categories.map(item => {
+    const listItems = categories.map((item, index) => {
       return(
         <li
           className="nav-item nav-link"
+          key={index}
           id={item}
           onClick={(e) => this.changeStyleNavItem(e)}
-          ref={item}
+          ref={item => this.navItemRef[index] = item}
         >
           {item.toUpperCase()}
         </li>
